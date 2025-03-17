a1034fba3c09318b040a3ec26a157a588eeb162a
VolumiDev <diego.martga@gmail.com>
2025-03-17 22:40:09 +0100

    iniciamo proyecto y login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./auth/components/login/login.component')
+    .then((c) => c.LoginComponent) 
+  }
+
+];
---
+++ src/app/auth/components/login/login.component.ts
@@ -0,0 +1,44 @@
+import { Component, inject } from '@angular/core';
+import { FormsModule, ReactiveFormsModule, UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [RouterLink, FormsModule, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styles: `
+    :host {
+      display: block;
+    }
+  `
+})
+export class LoginComponent {
+  signInForm!: UntypedFormGroup
+  submitted: boolean = false
+
+  public fb = inject(UntypedFormBuilder)
+  // public store = inject(Store)
+
+  ngOnInit(): void {
+    this.signInForm = this.fb.group({
+      email: ['user@demo.com', [Validators.required, Validators.email]],
+      password: ['123456', [Validators.required]],
+    })
+  }
+
+  get formValues() {
+    return this.signInForm.controls
+  }
+
+  login() {
+    this.submitted = true
+    if (this.signInForm.valid) {
+      const email = this.formValues['email'].value
+      const password = this.formValues['password'].value
+
+      // Login Api
+      // this.store.dispatch(login({ email: email, password: password }))
+    }
+  }
+
+}
