88d79663eff9f1e544c24a96e6b2f082f0322119
VolumiDev <diego.martga@gmail.com>
2025-03-18 13:23:01 +0000

    Dev container para FrontPequeGestion

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -4,7 +4,7 @@ import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink, FormsModule, ReactiveFormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styles: `
     :host {
