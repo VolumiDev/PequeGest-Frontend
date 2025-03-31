e16a58eda139d39983b969f28a76f9bff0b56c7d
VolumiDev <diego.martga@gmail.com>
2025-03-31 22:23:03 +0200

    formulario de alta de alumnos. formulario para añadir a los papas

--- src/app/pages/users-page/components/students/studentDetailForm/studentDetailForm.component.ts
+++ src/app/pages/users-page/components/students/studentDetailForm/studentDetailForm.component.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core';
+import { ParentFormComponent } from "../parentForm/parentForm.component";
 
 @Component({
   selector: 'app-student-detail-form',
-  imports: [],
+  imports: [ParentFormComponent],
   templateUrl: './studentDetailForm.component.html',
 })
-export class StudentDetailFormComponent { }
+export class StudentDetailFormComponent {
+  
+  isVisibleParentForm: boolean = false;
+  
+  
+  changeParentFormVisibility(event: Event) {
+    event?.preventDefault()
+    if(this.isVisibleParentForm){
+      this.isVisibleParentForm = false;
+    }else{
+      this.isVisibleParentForm = true;
+    }
+    console.log(this.isVisibleParentForm);
+    
+
+  }
+
+}
